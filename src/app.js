522984cbbe1a2e978435345dd596e8f8830270aa
pamsflying <pam.miller61@gmail.com>
2020-08-30 15:44:01 -0500

    Fixed GitHub link

--- src/app.js
+++ src/app.js
@@ -97,11 +97,7 @@ function displayForecast(response) {
       <h3>
         ${formatHours(forecast.dt * 1000)}
       </h3>
-      <img
-        src="http://openweathermap.org/img/wn/${
-          forecast.weather[0].icon
-        }@2x.png"
-      />
+      <img src="" id="icon" />
       <div class="weather-forecast-temperature">
         <strong>
           ${Math.round(forecast.main.temp_max)}°
