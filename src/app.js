5dc458478ce50beb86219ceee5d3a0382f1ac6ad
pamsflying <pam.miller61@gmail.com>
2020-09-07 19:42:51 -0500

    Forecast now updates with current location

--- src/app.js
+++ src/app.js
@@ -135,11 +135,14 @@ function handleSubmit(event) {
   search(cityInputElement.value);
 }
 
-//current location API and button
+//Geo location API and button
 function searchLocation(position) {
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function getCurrentLocation(event) {
